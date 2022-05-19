3ea6f8fde090f17a0938624b3fdd7b91db269bf4
dipterix <dipterix.wang@gmail.com>
2022-05-19 17:44:56 -0400

    Added pipeline_run_bare to always wait for the results and raise errors (if errored)

--- R/pipeline-run.R
+++ R/pipeline-run.R
@@ -162,3 +162,111 @@ pipeline_run <- function(
   res
 }
 
+
+#' @rdname rave-pipeline
+#' @export
+pipeline_run_bare <- function(
+  pipe_dir = Sys.getenv("RAVE_PIPELINE", "."),
+  scheduler = c("none", "future", "clustermq"),
+  type = c("smart", "callr", "vanilla"),
+  envir = new.env(parent = globalenv()),
+  callr_function = NULL,
+  names = NULL,
+  ...) {
+  pipe_dir <- activate_pipeline(pipe_dir)
+
+  type <- match.arg(type)
+  scheduler <- match.arg(scheduler)
+  callr_function <- substitute(callr_function)
+
+
+  clustermq_scheduler <- getOption('clustermq.scheduler', NA)
+  if(scheduler == "clustermq"){
+    # if(!identical(clustermq_scheduler, "LOCAL")){
+    #   callr_function <- NULL
+    # }
+    callr_function <- NULL
+  }
+
+  if(type == "vanilla"){
+    callr_function <- NULL
+  } else if (type == "callr") {
+    callr_function <- quote(callr::r)
+  }
+
+  all_names <- pipeline_target_names(pipe_dir = pipe_dir)
+
+  if(length(names)) {
+    if(!is.character(names)){
+      stop("pipeline_run_bare: `names` must be NULL or characters")
+    }
+    missing_names <- names[!names %in% all_names]
+    if(length(missing_names)) {
+      stop("pipeline_run_bare: the following `names` cannot be found: ", paste(missing_names, collapse = ", "))
+    }
+  } else {
+    names <- NULL
+  }
+
+  if(type == "smart") {
+    local <- with_future_parallel
+  }
+
+  args <- list(
+    names = names,
+    envir = envir,
+    callr_function = callr_function,
+    ...
+  )
+
+  make <- function(fun, use_local = TRUE) {
+    tryCatch({
+      if( use_local ) {
+        local({ do.call(fun, args) })
+      } else {
+        do.call(fun, args)
+      }
+
+    }, `tar_condition_file` = function(e) {
+      # destroy and try again, and throw all other errors
+      targets::tar_destroy(ask = FALSE, destroy = "meta")
+      if( use_local ) {
+        local({ do.call(fun, args) })
+      } else {
+        do.call(fun, args)
+      }
+    })
+  }
+
+  switch (
+    scheduler,
+    "none" = {
+      make( targets::tar_make )
+    },
+    "future" = {
+      args$workers <- raveio_getopt("max_worker", default = 1L)
+      make( targets::tar_make_future )
+    },
+    {
+      if(is.na(clustermq_scheduler)) {
+        clustermq_scheduler <- "multiprocess"
+      }
+      options('clustermq.scheduler' = clustermq_scheduler)
+      if(identical(clustermq_scheduler, "LOCAL")){
+        make( targets::tar_make_clustermq )
+        # local({ do.call(targets::tar_make_clustermq, args) })
+      } else {
+        args$workers <- raveio_getopt("max_worker", default = 1L)
+        # do.call(targets::tar_make_clustermq, args)
+        make( targets::tar_make_clustermq, use_local = FALSE )
+      }
+    }
+  )
+
+  # Read in names
+  if(!length(names)) {
+    names <- pipeline_target_names(pipe_dir = pipe_dir)
+  }
+  pipeline_read(var_names = names, pipe_dir = pipe_dir)
+
+}
