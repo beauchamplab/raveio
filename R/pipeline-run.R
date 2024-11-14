51bb7d8b22293a8cfffc07930a7f45a220e97dd8
dipterix <dipterix.wang@gmail.com>
2024-11-14 18:02:49 -0500

    Make sure temporary folder exists before running pipelines

--- R/pipeline-run.R
+++ R/pipeline-run.R
@@ -95,6 +95,15 @@ pipeline_run <- function(
     }
     make <- function(fun, use_local = TRUE) {
       suppressWarnings({
+
+        # Make sure temporary folder exists before running pipelines
+        if(!dir.exists(tempdir())) {
+          tryCatch({
+            dir_create2(tempdir())
+          }, error = function(e) {})
+        }
+
+        # Make sure the temporary folder exists
         tryCatch(
           expr = {
             ns$pipeline_clean(destroy = "process")
