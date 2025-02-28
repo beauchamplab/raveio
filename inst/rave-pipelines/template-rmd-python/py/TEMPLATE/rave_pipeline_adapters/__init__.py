9486dab928a8334a271c5eb399f4c902fa8a6cd8
dipterix <dipterix.wang@gmail.com>
2025-02-28 18:01:57 -0500

    reduce name length

--- inst/rave-pipelines/template-rmd-python/py/TEMPLATE/rave_pipeline_adapters/__init__.py
+++ inst/rave-pipelines/template-rmd-python/py/TEMPLATE/rave_pipeline_adapters/__init__.py
@@ -1,5 +1,5 @@
 # Make sure the decorator `rave_target` is available to the sub-functions
-from .runtime_decorator import RAVERuntimeException, rave_target
+from .errors import RAVERuntimeException, rave_target
 
 # Make sure `rave_serialize` and `rave_unserialize` are available for RAVE pipeline
 from .serializers import rave_serialize, rave_unserialize
